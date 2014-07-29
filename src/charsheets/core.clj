1e3c42e05fd0eac41a5e609f06b39dab9eb6c80b
jphollanti <pikkuaapo@gmail.com>
2014-07-29 13:17:38 +0300

    Initial commit. Basic structure.

---
+++ src/charsheets/core.clj
@@ -0,0 +1,65 @@
+(ns charsheets.core)
+(require '[clj-yaml.core :as yaml])
+
+(defn weighted-rand-choice [m]
+  (let [w (reductions #(+ % %2) (vals m))
+        r (rand-int (last w))]
+    (nth (keys m) (count (take-while #( <= % r ) w)))))
+
+(defn deep-merge
+  "Recursively merges maps. If keys are not maps, the last value wins."
+  [& vals]
+  (if (every? map? vals)
+    (apply merge-with deep-merge vals)
+    (last vals)))
+
+(defn add-values
+  "Adds values to sheet."
+  [ordinals class sheet empty-sheet]
+    (doseq [ordinal ordinals]
+      (dorun
+        (for
+          [i (range (val ordinal))]
+
+          (let [rand-ordinal (rand-nth (keys (get (get empty-sheet class) (key ordinal))))]
+            (dosync
+              (alter sheet update-in
+                [class (key ordinal) rand-ordinal]
+                inc)
+              )
+            )
+
+          )
+        ))
+    )
+
+(defn generate
+  "Creates a new sheet."
+  [dichotomies]
+  (assert
+    (>
+      (count dichotomies) 4))
+
+  (let [empty-sheet (get
+                      (yaml/parse-string
+                        (slurp "./resources/sheet-information.yaml"))
+                      :sheet)
+        grouping-effects (get
+                           (yaml/parse-string
+                             (slurp "./resources/grouping-effects.yaml"))
+                           :entj)
+        sheet-tpl (deep-merge empty-sheet (get grouping-effects :sheet))
+        attributes-ordinal (get grouping-effects :attributes-ordinal)
+        abilities-ordinal (get grouping-effects :abilities-ordinal)]
+
+    (def sheet
+      (ref
+        sheet-tpl))
+
+    (add-values attributes-ordinal :attributes sheet empty-sheet)
+    (add-values abilities-ordinal :abilities sheet empty-sheet)
+
+    @sheet
+
+    ))
+
---
+++ test/charsheets/core_test.clj
@@ -0,0 +1,7 @@
+(ns charsheets.core-test
+  (:require [clojure.test :refer :all]
+            [charsheets.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
