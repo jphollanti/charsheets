bd2e3a4a4e6a141a09972d6e28364126d1fe8615
jphollanti <pikkuaapo@gmail.com>
2014-08-10 20:19:42 +0300

    Added unit test for get-map-with-values-in-limit.

--- test/charsheets/core_test.clj
+++ test/charsheets/core_test.clj
@@ -1,7 +1,7 @@
 (ns charsheets.core-test
   (:require [clojure.test :refer :all]
-            [charsheets.core :refer :all]))
+            [charsheets.core :refer :all :as charsheets]))
 
-(deftest a-test
+(deftest get-map-with-values-in-limit-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 1 (count (keys (charsheets/get-map-with-values-in-limit {:k1 4 :k2 5})))))))
